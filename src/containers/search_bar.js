f48c40fbef1a7efb14f376ec92b1fd1b5fc67e7e
rishindrareddy <rishindrareddy@hotmail.com>
2017-09-13 08:14:15 -0700

    connecting action in search_bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux'; // connects SearchBar container to redux so that this container can fire actions
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
   constructor(props){
     super(props);
 
@@ -9,15 +12,22 @@ export default class SearchBar extends Component{
     // binds onInputChange function with the instance object of SearchBar
     // now assign it to the function present in the SearchBar instance.
     this.onInputChange = this.onInputChange.bind(this);
+
+    // to bind the SearchBar object instance with the onFormSubmit function
+    // so that 'this' can be accessed in funciton scope
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event){
-    this.setState({term: event.target.value})
+    this.setState({term: event.target.value});
   }
 
   // fetch weather data
   onFormSubmit(event){
     event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({term:''});
   }
 
 
@@ -37,3 +47,13 @@ export default class SearchBar extends Component{
     );
   }
 }
+
+// to wire the fetchWeather function to the action.
+// we are bindind the action to this fetchWeather function
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+// 1st argument for state, so we are not mapping state to props so null
+// 2nd arg is about mapping dispatch to props
+export default connect(null, mapDispatchToProps)(SearchBar);
