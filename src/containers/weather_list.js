dcd31506d65feff53f1e307d6933a1b9b4113934
rishindrareddy <rishindrareddy@hotmail.com>
2017-09-13 22:48:41 -0700

    Adding weather list to app

---
+++ src/containers/weather_list.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class WeatherList extends Component {
+
+  render(){
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+        </table>
+    );
+  }
+}
+
+//
+function mapStateToProps({weather}){
+//  return {weather:state.weather};
+// above is equal to below
+// return {weather : weather}
+// above is equal to below
+
+return {weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import {weatherReducer} from './reducer_weather';
 
+// maps reducer_weather reducer to weather.
+// this is a centrl unit that manages reducers and maps to objects
+// The combineReducers helper function turns an object whose values
+// are different reducing functions into a single reducing function you can pass to createStore
 const rootReducer = combineReducers({
   weather : weatherReducer
 });
